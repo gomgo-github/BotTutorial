f06b79861cf6b0af69d2a6d8c97af32d13e1338c
BLUE UNITY <gomgounity@gmail.com>
2021-07-11 15:49:48 +0000

    fdg

--- Bot/settings.js
+++ Bot/settings.js
@@ -1,9 +1,9 @@
-const discord = require("discord.js");
-const client = new discord.Client;
+const Discord = require("discord.js");
+const client = new Discord.Client();
 const Token = "ODYzODAxMjE3NzYyNzIxODAy.YOsLkg.HbEJ4ipXcUnp6Fxcd0gZrJWtqk0";
 const prefix = ")";
 
 
 
 
-module.export = { discord, client, Token, prefix }
+module.export = { Discord, client, Token, prefix }
--- index.js
+++ index.js
@@ -1,17 +1,35 @@
+
 var http = require('http');  
 http.createServer(function (req, res) {   
   res.write("Bot funcionando con host en Replit \n Version: --");   
   res.end(); 
 }).listen(8080); //index.js
 
-const { discord, client, Token, prefix } = require("./Bot/setting.js");
+const { Discord, client, Token, prefix } = require("./Bot/settings.js");
 
+client.on('ready',() => {
+  console.log("Ya estoy vivo")
+  client.user.setPresence({
+    status: "dnd",
+    activity:{
+      name: "tuto!command para algo",
+      type: "PLAYING"
+    }
+  })
+})
 
+client.on('message', message => {
+    const args = message.content.slice(prefix.length).trim().split(' ');
+    const command = args.shift()
+    if (!message.content.startsWith(prefix) || message.author.bot || message.channel instanceof Discord.DMChannel ) return; // Leer Mensajes
 
+    switch(command.toLowerCase()){
+      case "help":
+        message.reply("Solo hay este comando\n\nPuedes recomendarme mas comandos")
+      break
+    }
 
-
-
-
+})
 
 
 
